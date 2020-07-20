83719ea9ad645be88c0e5dea2b8d4e9449dd9f53
sanyamarora0550 <65460009+sanyamarora0550@users.noreply.github.com>
2020-07-20 12:41:07 +0530

    Update users.sql

--- db/users.sql
+++ db/users.sql
@@ -1,25 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Jul 07, 2019 at 11:54 AM
--- Server version: 5.7.25
--- PHP Version: 7.2.14
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
---
--- Database: `nodejs`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -31,27 +9,14 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`) VALUES
-(3, 'hezhan', 'hezan@gmail.com'),
-(4, 'hezhans', 'hezan@gmail.com'),
-(5, 'heshanpw', 'heshamp@gmail.com'),
-(6, 'heshanpw', 'heshamp@gmail.com');
+(3, 'h', 'h@gmail.com'),
+(4, 'e', 'e@gmail.com'),
+(5, 's', 's@gmail.com'),
+(6, 'd', 'd@gmail.com');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
